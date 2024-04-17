cebeb6ea670111f6e343c4dbed8c80c6dff37077
autel_office <wbeeson@autelrobotics.com>
2024-04-17 14:19:35 -0700

    Finished

--- send_emails.py
+++ send_emails.py
@@ -4,144 +4,153 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 
+from email_validator import validate_email, EmailNotValidError
+import html2text
+import re
 
-def send_email(subject, text_body, html_body, sender, recipients: list, password, server):
+
+def get_validated_email(email):
+    try:
+        # validate and get info
+        validation_info = validate_email(email)
+
+        # replace with normalized form
+        email = validation_info.email
+        return email
+    except EmailNotValidError as e:
+        # email is not valid, exception message is human-readable
+
+        print(f"ERROR {email} [{str(e)}] Skipping to next entry...")
+        return None
+
+
+def send_email(server, email, subject, message, sender):
+    email = "wbeeson@autelrobotics.com"
     # Declare message root
     msg_root = MIMEMultipart("related")
     msg_root['Subject'] = subject
     msg_root['From'] = sender
-    msg_root['To'] = ', '.join(recipients)
+    msg_root['To'] = email
 
     # Assign message alternatives
     msg_alternative = MIMEMultipart('alternative')
     msg_root.attach(msg_alternative)
-    msg_alternative.attach(text_body)
-    msg_alternative.attach(html_body)
 
-    # Add image
-    fp = open('assets/logo.png', 'rb')
-    msg_image = MIMEImage(fp.read())
-    fp.close()
+    #  Add images to the HTML version
+    images = re.findall('<img [^>]*src="([^"]+)', message)
+    for i, image in enumerate(images):
+        #  Set the ID of the image in the text
+        message = message.replace(image, "cid:" + str(i))
 
-    # Define the image's ID as referenced in HTML text
-    msg_image.add_header('Content-ID', '<logo>')
-    msg_root.attach(msg_image)
+        #  Create an Email Image object
+        fp = open("assets/" + image, 'rb')
+        msg_image = MIMEImage(fp.read())
+        fp.close()
 
-    # Send message
-    server.sendmail(sender, recipients, msg_root.as_string())
+        # Define the image's ID as referenced in HTML text
+        msg_image.add_header('Content-ID', f'<{i}>')
+        msg_root.attach(msg_image)
 
+    #  Add plain text version to Email
+    text_version = html2text.html2text(message)
+    text_final = MIMEText(text_version, "plain")
+    msg_alternative.attach(text_final)
 
-def send_old_greeting_email(name, product, recipient, server):
-    # Step 1: Create the plain text variant
-    text = ("Dear {name},\n\nI am writing to follow up on an information query you made some time ago about our "
-            "{product}. I am checking in to make sure that you have found a solution that you are satisfied with. \n\n"
-            "What solution did you end up going with?\n\nSincerely,\n\nWill Beeson | Sales Representative\n"
-            "Direct +1 (425)780-8024\nwww.autelrobotics.com")
+    #  Add HTML version to Email
+    html_final = MIMEText(message, "html")
+    msg_alternative.attach(html_final)
 
-    # Step 2: Create the plain html variant
-    html = """\
-    <html>
-      <body>
-        <p>Dear {name},</p>
-        <p>I am writing to follow up on an information query you made some time ago about our {product}. I am checking in 
-            to make sure that you have found a solution that you are satisfied with.</p>
-        <p>What solution did you end up going with?</p>
-        <p>Sincerely,</p>
-        <p><b>Will Beeson | Sales Representative</b><br>Direct +1 (425)780-8024<br><img src="cid:logo"><br>www.autelrobotics.com</p>
-      </body>
-    </html>
-    """
-
-    # Step 3: Set email information
-    text = text.format(name=name, product=product)
-    html = html.format(name=name, product=product)
-    set_html_body = MIMEText(html, "html")
-    set_text_body = MIMEText(text, "plain")
-
-    subject = "Drone Information Follow-up"
-    send_email(subject, set_text_body, set_html_body, sender, recipient, password, server)
+    server.sendmail(sender, [email], msg_root.as_string())
 
 
-def send_livedeck_promotion_email(name, dealer, recipient, server):
-    # Step 1: Create the plain text variant
-    text = (
-        "Dear {name},\n\n"
+def validate_message(column_names):
+    html_template = open("assets/email_message.html").read()
 
-        "I hope this message finds you well. I'm excited to share an exclusive offer with {dealer} as a "
-        "valued partner of Autel Robotics. For a limited time only, we're thrilled to provide you with a special "
-        "offer: with every purchase of an Autel Robotics EVO II 640T V3 series drone, you'll receive a "
-        "complimentary LiveDeck 2.\n\n"
+    #  Check to make sure user submitted the correct tag pairs
+    tags = [tag.lower().strip() for tag in re.findall(r"\{(.*?)\}", html_template)]
+    if sorted(tags) != sorted(column_names):
+        while True:
+            print("Detected tags are different than the tags defined in the CSV file.")
+            print(f"CSV:  [{sorted(column_names)}]")
+            print(f"HTML: [{sorted(tags)}]")
+            response = input("Continue? (y/n)")
+            if response.lower().__contains__("y"):
+                break
+            if response.lower().__contains__("n"):
+                raise Exception("User stopped the program. Revise tags and re-run.")
 
-        "The LiveDeck 2 seamlessly integrates with the EVO II 640T V3 drone to deliver high-quality, real-time video "
-        "streaming capabilities without requiring any wifi or cellular activity to use. \n\n"
-
-        "This offer is available until May 1st. Contact us today to place your order and take advantage of this "
-        "special deal! This offer cannot be combined with any other offer. \n\n"
-
-
-        "Thank you for your continued partnership with Autel Robotics. We're committed to supporting you and helping "
-        "you succeed in meeting your customers' needs and expectations.\n\n"
-
-        "Will Beeson | Sales Representative\n"
-        "www.autelrobotics.com")
-
-    # Step 2: Create the html variant
-    html = """\
-    <html>
-      <body>
-        <p>Dear {name},</p>
-
-        <p>I hope this message finds you well. I'm excited to share an exclusive offer with {dealer} as a valued partner 
-        of Autel Robotics. For a limited time only, we're thrilled to provide you with a special offer: with every 
-        purchase of an Autel Robotics EVO II 640T V3 series drone, you'll receive a complimentary LiveDeck 2.</p>
-
-        <p>The LiveDeck 2 seamlessly integrates with the EVO II 640T V3 drone to deliver high-quality, real-time video streaming 
-        capabilities without requiring any wifi or cellular activity to use. </p>
-
-        <p>This offer is available until May 1st. Contact us today to place your order and take advantage of this 
-        special deal! This offer cannot be combined with any other offer. </p>
-
-        <p>Thank you for your continued partnership with Autel Robotics. We're committed to supporting you and helping 
-        you succeed in meeting your customers' needs and expectations.</p>
-        
-        <p><a href="https://drive.google.com/drive/folders/1m1GO5SwB82gZkI4CVvZ1b8aGZRSuR2HX">LiveDeck 2 Information</a><br>
-        <a href="https://drive.google.com/drive/folders/1-CxA0Q-Ch2s4YMNu5sUiVLTWBqEhc5K7">EVO II 640T Information</a>
-        </p>
-        
-        <p><img src="cid:logo" width: 30%;><b><br>
-        Will Beeson | Sales Representative</b><br>
-      </body>
-    </html>
-    """
-
-    # Step 3: Set email information
-    text = text.format(name=name, dealer=dealer)
-    html = html.format(name=name, dealer=dealer)
-    set_html_body = MIMEText(html, "html")
-    set_text_body = MIMEText(text, "plain")
-
-    subject = "Autel Offer: Free LiveDeck 2 with Each EVO II Purchase!"
-    send_email(subject, set_text_body, set_html_body, sender, recipient, password, server)
+    return html_template
 
 
-with open('keys/will_email.json') as f:
-    file = json.load(f)
-    sender = file["username"]
-    password = file["password"]
+def customize_message(template, column_names, column_values):
+    for i, tag in enumerate(column_names):
+        template = template.replace("{" + tag + "}", column_values[i].title())
+    return template
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp_server:
-    smtp_server.login(sender, password)
-    with open('assets/dealer_list.csv', "r", encoding="UTF-8") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for i, row in enumerate(csv_reader):
-            if i == 0: continue
-            dealer = row[0]
-            name = row[1]
-            email = row[2]
-            send_livedeck_promotion_email(name=name, dealer=dealer, recipient=[email], server=smtp_server)
-            print(f"Message Sent To: {email} [Row {i}]")
 
-with open('assets/dealer_list.csv') as f:
+def get_row_values(rows):
+    values = []
+    for i in range(len(rows)):
+        values.append(rows[i].lower().strip())
+    return values
+
+
+def start_server():
+    with open('keys/email.json') as f:
+        file = json.load(f)
+        sender = file["username"]
+        password = file["password"]
+
+    #  Start the email server
+    context = ssl.create_default_context()
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context)
+    server.login(sender, password)
+    return server, sender
+
+
+def get_column_names(csv_reader):
+    #  Check the first line for the required email field
+    column_names = get_row_values(next(csv_reader))
+    if "email" not in column_names:
+        raise Exception(f"Your top line header labels of the CSV file must contain the field 'email'. "
+                        f"Detected fields: {column_names}")
+
+    #  Remove the email field as it is hardcoded for different functionality than the rest of them
+    email_index = column_names.index("email")
+    column_names.pop(email_index)
+    return email_index, column_names
+
+
+#  Start the server
+smtp_server, sender = start_server()
+
+#  Load the contact information
+csv_file = open('assets/list.csv', "r", encoding="UTF-8")
+csv_reader = csv.reader(csv_file, delimiter=',')
+
+#  Get the column names
+email_index, column_names = get_column_names(csv_reader)
+template = validate_message(column_names)
+
+#  Get the subject
+subject = input("Please enter the subject line: ")
+
+#  Send the Emails
+for i, rows in enumerate(csv_reader):
+    column_values = get_row_values(rows)
+    email = get_validated_email(column_values.pop(email_index))
+    if email is None:
+        continue
+    get_customized_message = customize_message(template, column_names, column_values)
+    send_email(
+        server=smtp_server,
+        email=email,
+        subject=subject,
+        message=get_customized_message,
+        sender=sender
+    )
+    print(f"Message Sent To: {email} [Row {i}]")
+
+with open('assets/list.csv') as f:
     row_count = sum(1 for line in f) - 1
 print(f"Done: Send Emails to all {row_count} Contacts")
